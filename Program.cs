ec0f870c4929a1b1c25117e5badb314b3509ceab
itsWindows11 <itswin11@outlook.com>
2023-07-05 14:58:46 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace WMIEventLoggerTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write something to log: ");
+            var str = Console.ReadLine();
+
+            Console.Write("\nWrite a log type: ");
+            var logType = Console.ReadLine();
+
+            var wmiLogger = new WMIEventLogger();
+
+            switch (logType)
+            {
+                case "Information":
+                    wmiLogger.LogInformation(str);
+                    break;
+                case "Error":
+                    wmiLogger.LogError(str);
+                    break;
+                case "Warning":
+                    wmiLogger.LogWarning(str);
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WMIEventLogger.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace WMIEventLoggerTest;
+
+public sealed class WMIEventLogger : ILogger
+{
+    public IDisposable? BeginScope<TState>(TState state) where TState : notnull => default!;
+
+    public bool IsEnabled(LogLevel logLevel) => true;
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+    {
+        if (!IsEnabled(logLevel)) return;
+
+        var type = logLevel switch
+        {
+            LogLevel.Warning => EventLogEntryType.Warning,
+            LogLevel.Error or LogLevel.Critical => EventLogEntryType.Error,
+            _ => EventLogEntryType.Information,
+        };
+
+        EventLog.WriteEntry("Logger Test App", formatter(state, exception), type);
+    }
+}
